8897c5ece8ef3cbb745c73375e57d074b83a2154
laisrmacedo <laisrodriguesmacedo@gmail.com>
2022-07-20 23:21:43 +0200

    Inicio

---
+++ main.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = "Lais Macedo"
+var vel = 100;
+
+function digitacao(){
+    if(i<txt.length){
+        document.getElementById("texto").innerHTML = "funcionou"
+        // i++
+        // setTimeout(digitacao,vel);
+    }    
+}
+
+document.getElementById("texto").innerHTML = "funcionou"
